d59d1a5e4ab801c3714b21429f06edccbf137d2f
MouliDharan <82305609+Moulidharan16@users.noreply.github.com>
2022-07-21 22:27:07 +0530

    Update jenkinsjava.java

--- jenkinsjava.java
+++ jenkinsjava.java
@@ -2,5 +2,6 @@ public class jenkinsjava {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
    System.out.println("Welome jenkins"); 
+        System.out.println("Welome mouli"); 
     }
 }
